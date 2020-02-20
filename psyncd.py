78a80bc6236a79bc9b9665740eba749e3a06f75c
zkeeer <zangker@foxmail.com>
2020-02-20 17:20:16 +0800

    v1.3

--- psyncd.py
+++ psyncd.py
@@ -169,46 +169,46 @@ class Psyncd:
                         self.rsync_command_list.remove(rsync_command)
 
     def is_syncing(self):
-	    """check if is rsync process running"""
-	    cm_status, cm_output = commands.getstatusoutput("ps aux|grep 'rsync'")
-		if cm_status == 0:
-		    cm_output = cm_output.split('\n')
-			for item in cm_output:
-			    if "grep rsync" not in item and "ps aux" not in item:
-				    return True
-		return False
+        """check if is rsync process running"""
+        cm_status, cm_output = commands.getstatusoutput("ps aux|grep 'rsync'")
+        if cm_status == 0:
+            cm_output = cm_output.split('\n')
+            for item in cm_output:
+                if "grep rsync" not in item and "ps aux" not in item:
+                    return True
+        return False
 
     def sync_file_delete(self):
-	    """sync files deleted"""
+        """sync files deleted"""
         print "start sync file(delete)"
         while True:
-		    # wait for file change message
+            # wait for file change message
             while not FileDeleteList:
                 time.sleep(1)
-			# wait for producter
+            # wait for producter
             while FILEDELETELOCK:
                 time.sleep(0.1)
-			# wait for sync process
-			# sync delete files after created/modified
-			while self.is_syncing():
-			    time.sleep(1)
-			# let producter wait
+            # wait for sync process
+            # sync delete files after created/modified
+            while self.is_syncing():
+                time.sleep(1)
+            # let producter wait
             FILEDELETELOCK = True
             tmplist = FileDeleteList
             FileDeleteList = []
             FILEDELETELOCK = False
             # get absolute path
             path_list = []
-			for item in tmplist:
-			    # item : directory#deleted#filename
-				# filepath : /home/zkeeer/approot/backend/test
-			    filepath = item.split("#")[2]
-				path_split_list = filepath.split("/")
-				if len(path_split_list) > 2:
-				    path_split_list.pop(-1)
-				tmpresult = "/"+"/".join(path_split_list)
-				path_list.append(tmpresult)
-			
+            for item in tmplist:
+                # item : directory#deleted#filename
+                # filepath : /home/zkeeer/approot/backend/test
+                filepath = item.split("#")[2]
+                path_split_list = filepath.split("/")
+                if len(path_split_list) > 2:
+                    path_split_list.pop(-1)
+                tmpresult = "/"+"/".join(path_split_list)
+                path_list.append(tmpresult)
+            
             # get top path
             path_list.sort(key=lambda item: len(item))
             rpath_list = path_list
@@ -218,9 +218,9 @@ class Psyncd:
                     if item in item_sub:
                         path_list.remove(item_sub)
                         rpath_list.remove(item_sub)
-			# sync
+            # sync
             for path_item in path_list:
-			    for config_item in self.module_config_list:
+                for config_item in self.module_config_list:
                 source_path = config_item.get("source", None)
                 if source_path and source_path in path_item:
                     print("{} {}".format(time.ctime(), sync_file))
@@ -280,7 +280,7 @@ class Psyncd:
         watch_file_thread = Thread(target=self.watch_file_change_new)
         # sync_file_thread = Thread(target=self.sync_file)
         sync_file_thread_list = []
-		sync_file_delete_thread = Thread(target=self.sync_file_delete)
+        sync_file_delete_thread = Thread(target=self.sync_file_delete)
         for index in range(self.max_process):
             sync_file_thread_list.append(Thread(target=self.sync_file))
 
@@ -288,13 +288,13 @@ class Psyncd:
         # sync_file_thread.start()
         for item in sync_file_thread_list:
             item.start()
-		sync_file_delete_thread.start()
+        sync_file_delete_thread.start()
 
         watch_file_thread.join()
         # sync_file_thread.join()
         for item in sync_file_thread_list:
             item.join()
-		sync_file_delete_thread.join()
+        sync_file_delete_thread.join()
 
 
 if __name__ == '__main__':
